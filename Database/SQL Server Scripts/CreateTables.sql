1e4b2caaa0e25d283f61c90fda27033632291cbe
Marcus Vinicius <marcusvpsilveira@gmail.com>
2026-06-06 21:20:48 -0300

    Table tState created

--- Database/SQL Server Scripts/CreateTables.sql
+++ Database/SQL Server Scripts/CreateTables.sql
@@ -1,3 +1,55 @@
-USE PUCOMEX
+/*------------------------------------------------------------
+Author   : Marcus Paiva
+DataBase : PUCOMEX
+Objective: Create a software that supports brokers
+Date	 : 06/06/2026
+------------------------------------------------------------*/
+CREATE DATABASE PUCOMEX
+ON
+(
+    NAME = PUCOMEX_Data,
+    FILENAME = 'C:\Dados\PUCOMEX.mdf',
+    SIZE = 100MB,
+    MAXSIZE = UNLIMITED,
+    FILEGROWTH = 10MB
+)
+LOG ON
+(
+    NAME = PUCOMEX_Log,
+    FILENAME = 'C:\Log\PUCOMEX.ldf',
+    SIZE = 50MB,
+    MAXSIZE = 1GB,
+    FILEGROWTH = 5MB
+);
 GO
 
+/*------------------------------------------------------------
+Author   : Marcus Paiva
+DataBase : PUCOMEX
+Objective: Criaçao das tabelas
+Date	 : 06/06/2026
+------------------------------------------------------------*/
+--------------------------------------------------------------------------
+-- Tabela: tStates
+--------------------------------------------------------------------------
+CREATE SEQUENCE seqiStateId
+AS INT
+START WITH 1
+INCREMENT BY 1;
+GO
+
+CREATE TABLE tState
+(
+	iStateid INT NOT NULL
+		DEFAULT NEXT VALUE FOR seqiStateId,
+
+	cCode CHAR(2) NOT NULL,
+	cName VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_State
+		PRIMARY KEY (iStateid),
+
+	CONSTRAINT UQ_State_Code
+		UNIQUE (cCode)
+);
+GO
