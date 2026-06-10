094e48c3bf5190b1ce1c8c044bce90c98245d368
Marcus Vinicius <marcusvpsilveira@gmail.com>
2026-06-09 22:12:23 -0300

    All tables created

--- Database/CreateTable.sql
+++ Database/CreateTable.sql
@@ -90,7 +90,7 @@ GO
 
 CREATE TABLE tCustomer
 (
-	iCustomerId INT NOT NULL,
+	iCustomerId INT NOT NULL
 	DEFAULT NEXT VALUE FOR seqiCostumerId,
 
 	cTaxId VARCHAR(14) NOT NULL,
@@ -107,5 +107,100 @@ CREATE TABLE tCustomer
 
 	CONSTRAINT FK_CUSTOMER_CITY
 		FOREIGN KEY (iCityId) REFERENCES tCity (iCityId)
+);
+GO
+
+--------------------------------------------------------------------------
+-- Table: tImportProcess
+--------------------------------------------------------------------------
+CREATE SEQUENCE seqiImportProcessId
+AS INT
+START WITH 1
+INCREMENT BY 1;
+GO
+
+CREATE TABLE tImportProcess
+(
+	iImportProcessId INT NOT NULL
+		DEFAULT NEXT VALUE FOR seqiImportProcessId,
+
+	iCostumerId INT NOT NULL,
+
+	cProcessNumber VARCHAR(30) NOT NULL,
+	cCustomerReference VARCHAR(50) NULL,
+	cDuimpNumber VARCHAR(30) NULL,
+
+	dCreatedAt DATETIME NOT NULL
+		DEFAULT GETDATE(),
+
+	CONSTRAINT PK_IMPORT_PROCESS_ID
+		PRIMARY KEY (iImportProcessId),
+
+	CONSTRAINT UQ_PROCESS_NUMBER
+		UNIQUE (cProcessNumber),
+
+	CONSTRAINT FK_IMPORT_PROCESS_CUSTOMER
+		FOREIGN KEY (iCostumerId) REFERENCES tCustomer (iCustomerId)
+);
+GO
+
+--------------------------------------------------------------------------
+-- Table: tProcessHistory
+--------------------------------------------------------------------------
+CREATE SEQUENCE seqiProcessHistoryId
+AS INT
+START WITH 1
+INCREMENT BY 1;
+GO
+
+CREATE TABLE tProcessHistory
+(
+    iProcessHistoryId INT NOT NULL
+        DEFAULT NEXT VALUE FOR seqiProcessHistoryId,
+
+    iImportProcessId INT NOT NULL,
+
+    dtEvent DATETIME NOT NULL,
+
+    cEventDescription VARCHAR(500) NOT NULL,
+
+    CONSTRAINT PK_PROCESS_HISTORY_ID
+        PRIMARY KEY (iProcessHistoryId),
+
+    CONSTRAINT FK_PROCESS_HISTORY_PROCESS
+        FOREIGN KEY (iImportProcessId)
+        REFERENCES tImportProcess (iImportProcessId)
+);
+GO
+
+--------------------------------------------------------------------------
+-- Table: tDocument
+--------------------------------------------------------------------------
+CREATE SEQUENCE seqDocumentId
+AS INT
+START WITH 1
+INCREMENT BY 1;
+GO
+
+CREATE TABLE tDocument
+(
+    iDocumentId INT NOT NULL
+        DEFAULT NEXT VALUE FOR seqDocumentId,
+
+    iImportProcessId INT NOT NULL,
+
+    cDocumentType VARCHAR(50) NOT NULL,
+    cFileName VARCHAR(255) NOT NULL,
+    cFilePath VARCHAR(500) NOT NULL,
+
+    dtUploadDate DATETIME NOT NULL
+        DEFAULT GETDATE(),
+
+    CONSTRAINT PK_DOCUMENT_ID
+        PRIMARY KEY (iDocumentId),
+
+    CONSTRAINT FK_DOCUMENT_PROCESS
+        FOREIGN KEY (iImportProcessId)
+        REFERENCES tImportProcess (iImportProcessId)
 );
 GO
